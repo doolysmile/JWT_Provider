1aa9ea969d88a2ca3d4faf1db707e3c9782a95d0
KCH <doolysmile1@naver.com>
2022-10-12 22:28:18 +0900

    initial settings

---
+++ src/main/java/com/ll/exam/JWT_Provider/JwtProviderApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.JWT_Provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/JWT_Provider/JwtProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.JWT_Provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
