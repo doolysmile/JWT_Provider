19cd2022ff56e0c8ce7db258381a62b8752b16dc
KCH <doolysmile1@naver.com>
2022-11-29 18:26:11 +0900

    feat : JwtProvider 객체 생성 및 테스트 코드 작성

---
+++ src/main/java/com/ll/exam/JWT_Provider/app/JwtProvider.java
@@ -0,0 +1,18 @@
+package com.ll.exam.JWT_Provider.app;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+@Component
+public class JwtProvider {
+    @Value("${custom.jwt.secretKey}")
+    private String secretKeyPlain;
+
+    public SecretKey getSecretKey() {
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+        return Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());}
+}
--- src/test/java/com/ll/exam/JWT_Provider/JwtProviderApplicationTests.java
+++ src/test/java/com/ll/exam/JWT_Provider/JwtProviderApplicationTests.java
@@ -1,8 +1,10 @@
 package com.ll.exam.JWT_Provider;
 
+import com.ll.exam.JWT_Provider.app.JwtProvider;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,6 +15,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtProviderApplicationTests {
+
+	@Autowired
+	private JwtProvider jwtProvider;
 
 	@Test
 	void contextLoads() {
@@ -33,6 +38,14 @@ class JwtProviderApplicationTests {
 		String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
 		// hmac 암호화 알고리즘으로 SecretKey 객체 생성
 		SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+
+		assertThat(secretKey).isNotNull();
+	}
+
+	@Test
+	@DisplayName("JwtProvider 객체로 시크릿키 객체를 생성할 수 있다.")
+	void t3() {
+		SecretKey secretKey = jwtProvider.getSecretKey();
 
 		assertThat(secretKey).isNotNull();
 	}
